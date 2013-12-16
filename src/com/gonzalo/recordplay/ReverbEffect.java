d1ff94996fad4d837eb6af50a9faa2198c03767a
stecandr <stecandr@espol.edu.ec>
2013-12-16 14:28:16 -0500

    Edicion clase Reverb

--- src/com/gonzalo/recordplay/ReverbEffect.java
+++ src/com/gonzalo/recordplay/ReverbEffect.java
@@ -1,5 +1,21 @@
 package com.gonzalo.recordplay;
 
 public class ReverbEffect {
-
+	private static int tiempo=1;
+	//private static float decay=0.3f;
+	
+	public static double[] ReverbEffect(double[] audio, int audioSize, int fs){
+		int M = fs*tiempo;
+		int d, e;
+		int k=1;
+		Convolution conv = new Convolution();
+		double[] out = new double[audioSize];
+		double[] kernel= new double[M];
+		for(int i=0; i<M; i++){
+			kernel[i]=0f;
+		}
+		
+		out=conv.convolute(audio, kernel);
+		return out;
+	}
 }
